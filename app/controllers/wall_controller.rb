0e8be6cf869fc2563973c9bb52884d7173a48d87
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-17 11:34:19 -0800

    Added Bootstrap, simple_form, Hirb, and Rails_Footnotes

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -2,12 +2,18 @@ class WallController < ApplicationController
   def index
   	@user = User.find(current_user)
   	@posts = Post.all.reverse
+  	@post = Post.new
   end
 
   def message
   end
 
   def post
-  	@post = Post.create(user:current_user, content:params[:content])
+  	@post = Post.create(user:current_user, content:params[:post][:content])
+  	if @post.save
+  		render text: 'Your post was successfull!'
+  	else
+  		render @post.errors.full_messages
+  	end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'wall/index'
   get 'wall/message'
-  get 'wall/post'
+  post 'wall/post' => 'wall#post'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
